0a93e6393af239dc50fd517ccc6afad227933ce4
jenil59 <jenilsaliya1234@gmail.com>
2023-07-02 11:07:45 +0530

    data ingestion working"

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,6 @@ import sys
 import logging
 
 def error_message_details(error,error_detail:sys):
-
     _,_,exc_tb = error_detail.exc_info()
 
     file_name  = exc_tb.tb_frame.f_code.co_filename
@@ -19,3 +18,9 @@ class CustomException(Exception):
     def __str__(self) -> str:
         return self.error_message
 
+# if __name__ =='__main__':
+#     try:
+#         a = 3/0
+#     except Exception as e:
+#         raise CustomException(e,sys)
+    
